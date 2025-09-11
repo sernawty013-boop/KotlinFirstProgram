6180866ce12f373dc89a0f06aa912c3f9389eeee
sernawty013-boop <sernawty013@gmail.com>
2025-09-11 18:18:08 +0700

    Contoh dynamicvar

--- src/VarStatic.kt
+++
@@ -1,7 +0,0 @@
-fun main(args: Array<String>){
-    var dynamicVar = 2
-    println("contoh dynamic variabel = " + dynamicVar)
-
-    var staticVar: Char = 'A'
-    println("contoh static variabel = " + staticVar)
-}
