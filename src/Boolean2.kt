d5b34b18a0381a0714205264e940999eeb2ec554
sernawty013-boop <sernawty013@gmail.com>
2025-09-16 08:17:32 +0700

    Contoh String1

--- src/Boolean2.kt
+++ src/Boolean2.kt
@@ -1,8 +1,9 @@
 fun main(args: Array<String>) {
-    var varBool : Boolean = true
-    if(varBool)
-        println("varBool bernilai " + varBool)
-    var varInt : Int = 1
-    if(varInt)
-        println("varInt bernilai " + varInt)
+    var varBool: Boolean = true
+    if (varBool)
+        println("varBool bernilai $varBool")
+
+    var varInt: Int = 1
+    if (varInt > 0)   // perbaikan: ubah jadi kondisi Boolean
+        println("varInt bernilai $varInt")
 }
